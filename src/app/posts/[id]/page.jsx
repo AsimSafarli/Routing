02913f3ae379f88dc999a32b235dc420ca0c789a
AsimSafarli <asimseferli014@gmail.com>
2023-10-16 14:21:17 +0400

    addd new project

---
+++ src/app/posts/[id]/page.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    return notFound();
+  }
+  return res.json();
+}
+
+export async function generateMetadata({ params }) {
+  const post = await getData(params.id);
+  return {
+    title: post.title,
+    description: post.body,
+  };
+}
+const BlogPost = async ({ params }) => {
+  const data = await getData(params.id);
+  return (
+    <div className="flex flex-col ">
+      <div>{data.title}</div>
+      <div>{data.body}</div>
+    </div>
+  );
+};
+
+export default BlogPost;
---
+++ src/app/posts/page.jsx
@@ -0,0 +1,34 @@
+"use client"
+import axios from 'axios';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+
+const Post = () => {
+  const [posts, setPosts] = useState([]);
+  const baseURL = "https://jsonplaceholder.typicode.com/posts";
+  useEffect(() => {
+    axios.get(baseURL)
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+  return (
+    <div>
+      {posts.map((item) => (
+        <Link href={`/posts/${item.id}`} key={item.id} className='flex flex-row border border-solid border-gray-400 hover:shadow-lg w-max p-1 rounded-md'>
+          <div>
+          {/* <Image src={item.image} alt={item.title} width={200} height={300} /> */}
+            <h1>{item.title}</h1>
+            <p>{item.slug}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Post;
